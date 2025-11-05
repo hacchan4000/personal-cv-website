a4eab56f8dd01ae76508b8eba842317213ef9d24
Chandra <adityanugraha7251@gmail.com>
2025-11-05 10:42:13 +0800

    konfiguras asset

---
+++ my-app/public/Assets/assets.js
@@ -0,0 +1,96 @@
+//import asset gambar dekorasi
+import ad1 from './ad1.png';
+import ad2 from './ad2.png';
+import ad3 from './ad3.png';
+import adamHello from './ad3.png';
+import nookie from './nookie.PNG';
+
+//import gambar buat list skillsku
+import godot_logo from './Skills/Godot_logo.svg.png';
+import HTML5_logo from './Skills/HTML5_logo_and_wordmark.svg.png';
+import CSS_logo from './Skills/Official_CSS_Logo.svg.png';
+import Laravel_logo from './Skills/Laravel.svg.png';
+import Logo_Blender from './Skills/Logo_Blender.svg.png';
+import MySQL_logo from './Skills/MySQL_logo.svg.png';
+import Next_js_logo from './Skills/Next.js_wordmark.svg';
+import React_Logo from './Skills/React_Logo_SVG.svg.png';
+import PHP_Logo from './Skills/PHP-logo.svg.png';
+import Unity_logo from './Skills/Unity_2021.svg.png'
+
+
+export const assets = {
+    ad1,
+    ad2,
+    ad3,
+    adamHello,
+    nookie
+}
+export const skills = {
+    godot_logo,
+    HTML5_logo,
+    Laravel_logo,
+    Logo_Blender,
+    MySQL_logo,
+    Next_js_logo,
+    CSS_logo,
+    PHP_Logo,
+    React_Logo,
+    Unity_logo
+}
+
+export const projectsData = [
+    {
+        judul: '',
+        deskripsi: '',
+        gambar1: '',
+        gambar2: '',
+        gambar3: '',
+    },
+    {
+        judul: '',
+        deskripsi: '',
+        gambar1: '',
+        gambar2: '',
+        gambar3: '',
+    },
+    {
+        judul: '',
+        deskripsi: '',
+        gambar1: '',
+        gambar2: '',
+        gambar3: '',
+    },
+]
+
+export const experienceData = [
+    {
+        icon:'',
+        judul: 'GAME DEVELOPMENT',
+        deskripsi: 'lorem ipsum dolor sit amet',
+        link: ''
+    },
+    {
+        icon:'',
+        judul: 'WEB DEVELOPMENT',
+        deskripsi: 'lorem ipsum dolor sit amet',
+        link: ''
+    },
+    {
+        icon:'',
+        judul: 'DATA SCIENCE',
+        deskripsi: 'lorem ipsum dolor sit amet',
+        link: ''
+    },
+    {
+        icon:'',
+        judul: 'GRAPHIC DESIGN',
+        deskripsi: 'lorem ipsum dolor sit amet',
+        link: ''
+    },
+    {
+        icon:'',
+        judul: '3D DESIGN & ANIMATION',
+        deskripsi: 'lorem ipsum dolor sit amet',
+        link: ''
+    }
+]
