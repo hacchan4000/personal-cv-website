ba54932a089c6ceea19d9ef186ab64e4890140d4
Chandra <adityanugraha7251@gmail.com>
2025-11-14 11:21:50 +0800

    fix bgt?

--- my-app/app/components/Page4.jsx
+++ my-app/app/components/Page4.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { exp } from '@/public/Assets/assets'
+import Experience from './Experience.jsx'
 
 const Page4 = () => {
   return (
-    <div>
+    <div
+      id="experience"
+      className="relative w-full h-500 bg-white text-black flex flex-col items-center justify-center overflow-hidden rounded-4xl"
+    >
+      <h1
+        className="
+          mt-5 mb-10 
+          text-center font-extrabold 
+          text-[clamp(5rem,10vw,10rem)] 
+          tracking-tight
+        "
+      >
+        EXPERIENCE
+      </h1>
+
+        {exp.map((item, index) => (
+          <Experience
+            key={index}
+            id={item.id}
+            num={item.num}
+            judul={item.judul}
+            deskripsi={item.deskripsi}
+          />
+        ))}
       
     </div>
   )
